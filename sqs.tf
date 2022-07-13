84a8426fc3b8ae28a4bfdce19a7fdac57659f71d
Brendan Geraghty <105611121+b-geraghty@users.noreply.github.com>
2022-07-13 11:50:54 -0400

    Update sqs.tf

--- sqs.tf
+++ sqs.tf
@@ -2,7 +2,3 @@ resource "aws_sqs_queue" "test-queue" {
   name                       = "test-queue"
 }
 
-resource "aws_sqs_queue" "test-queue-2" {
-  name                       = "test-queue-2"
-}
-
